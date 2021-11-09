5dbe3ded7c1e1c3e72440f7359dac3c61e28773c
Owen Baldwin <owenabaldwin@gmail.com>
2021-11-09 14:29:21 +0000

    new and create done

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -8,6 +8,16 @@ class TasksController < ApplicationController
 
   end
 
+  def new
+    @task = Task.new
+  end
+
+  def create
+    @task = Task.new(task_params)
+    @task.save
+    redirect_to task_path(@task)
+  end
+
   private
 
   def task_params
